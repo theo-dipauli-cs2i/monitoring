0a19ca15d2a86cd6751b4d0dd4542bc63aed0d80
theod <theod@PHOQUEYOU>
2025-12-16 09:50:16 +0100

    test chart

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,33 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
+import Box from '@mui/material/Box';
+import { BarChart } from '@mui/x-charts/BarChart';
 
-export function App() {
-  const [count, setCount] = useState(0)
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = [
+  'Page A',
+  'Page B',
+  'Page C',
+  'Page D',
+  'Page E',
+  'Page F',
+  'Page G',
+];
 
+export default function App() {
   return (
     <>
-      <div>
+      <Box sx={{ width: '100%', height: 300 }}>
+        <BarChart
+          series={[
+            { data: pData, label: 'pv', id: 'pvId' },
+            { data: uData, label: 'uv', id: 'uvId' },
+          ]}
+          xAxis={[{ data: xLabels }]}
+          yAxis={[{ width: 50 }]}
+        />
+      </Box>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} class="logo" alt="Vite logo" />
         </a>
@@ -37,7 +56,7 @@ export function App() {
       </p>
       <p class="read-the-docs">
         Click on the Vite and Preact logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
